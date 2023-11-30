8fb951b3d80e67eccf66b08a93a236e08d05ceea
Tim Huitt <timhuitt@gmail.com>
2023-11-29 23:46:09 -0700

    fix loading indicator on landing page

--- public/js/script.js
+++ public/js/script.js
@@ -1,29 +1,30 @@
 
-const sidebarButton = document.querySelector('.sidebar-button')
-const sidebarFeed = document.querySelector('.feed')
-const postPartial = document.querySelector('.post-partial')
-const postButton = document.querySelector('.post-button')
-
-sidebarButton.addEventListener('click', () => {
-  sidebarButton.classList.toggle('hidden')
-  sidebarFeed.classList.toggle('hidden')
-})
-postButton.addEventListener('click', () => {
-  postButton.classList.toggle('hidden')
-  postPartial.classList.toggle('hidden')
-})
-
-
-
 document.addEventListener('DOMContentLoaded',() => {
   try {
     document.querySelectorAll('.async').forEach((element) => {
-        element.addEventListener('click', () => {
-            document.querySelector('.spinner').style.display = 'block';
+        element.addEventListener('click', (event) => {
+          const spinner = document.querySelector('.spinner')
+          spinner.style.display = 'block';
         });
     });
   } catch (err) {
     console.log(err)
   }
+
+  const sidebarButton = document.querySelector('.sidebar-button')
+  const sidebarFeed = document.querySelector('.feed')
+  const postPartial = document.querySelector('.post-partial')
+  const postButton = document.querySelector('.post-button')
+
+  sidebarButton.addEventListener('click', () => {
+    sidebarButton.classList.toggle('hidden')
+    sidebarFeed.classList.toggle('hidden')
+  })
+
+  postButton.addEventListener('click', () => {
+    postButton.classList.toggle('hidden')
+    postPartial.classList.toggle('hidden')
+  })
+
 });
 
