2515710fe6ab1e791ff99eb95fb968c155b2dbe3
SamPatt <git@sampatt.com>
2023-12-01 15:54:08 -0500

    further router cleanup

--- routes/posts.js
+++ routes/posts.js
@@ -3,19 +3,15 @@ var router = express.Router();
 
 const multer = require("multer");
 
-// multer - middleware -> code that runs between the inbound request and a server response 
-// multer will be used as 'inline middleware' 
-
 const upload = multer()
 
 var postsCtrl = require("../controller/posts");
 
-// for creating a post
+
 router.get("/", postsCtrl.index);
 router.post("/", upload.single('imageUpload'), postsCtrl.create);
 
-// for creating a comment
-// router.get('/new', postsCtrl.new)
+
 router.post("/:id/comments", postsCtrl.addComment);
 
 router.get("/:id", postsCtrl.show);
